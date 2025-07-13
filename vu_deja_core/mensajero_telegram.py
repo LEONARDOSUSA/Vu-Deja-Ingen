17ef02918d0d64ca594386536904f170ddb3dc02
LEONARDOSUSA <leonardosusa@hotmail.com>
2025-07-12 22:48:51 -0400

    Create mensajero_telegram.py

---
+++ vu_deja_core/mensajero_telegram.py
@@ -0,0 +1,13 @@
+import os
+import requests
+
+def enviar_mensaje_telegram(texto, chat_id):
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": texto,
+        "parse_mode": "Markdown"
+    }
+    response = requests.post(url, data=payload)
+    return response.status_code
